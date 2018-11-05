ef6baeb11807c7d070c05cd7b1bc44f724d9b006
jyjeong <jyjeong@mcnc.co.kr>
2018-11-05 21:47:45 +0900

    Oauth2 관련 라이브러리 추가, main application에 Oauth2 설정 추가

--- noteServer/note/src/main/kotlin/com/rabbitcat/note/NoteApplication.kt
+++ noteServer/note/src/main/kotlin/com/rabbitcat/note/NoteApplication.kt
@@ -2,7 +2,11 @@ package com.rabbitcat.note
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 
+@EnableResourceServer
+@EnableAuthorizationServer
 @SpringBootApplication
 class NoteApplication
 
