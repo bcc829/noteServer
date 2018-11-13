4368dec13554112718e6660a5a4671b7aee36653
jyjeong <jyjeong@mcnc.co.kr>
2018-11-13 12:58:45 +0900

    로컬 테스트를 위해 postgresql docker image 생성 방법 내용 추가

--- noteServer/note/src/main/resources/data.sql
+++ noteServer/note/src/main/resources/data.sql
@@ -1,5 +1,24 @@
 -- DROP SEQUENCE public.member_seq;
 
+--docker run -p 5432:5432 -e POSTGRES_PASSWORD=pass -e POSTGRES_USER=keesun -e POSTGRES_DB=springdata --name postgres_boot -d postgres
+--
+--docker exec -i -t postgres_boot bash
+--
+--su - postgres
+--
+--psql springdata
+--
+--데이터베이스 조회
+--\list
+--
+--테이블 조회
+--\dt
+--
+--쿼리
+--SELECT * FROM account;
+
+
+
 CREATE SEQUENCE public.member_seq
 NO MINVALUE
 NO MAXVALUE;
